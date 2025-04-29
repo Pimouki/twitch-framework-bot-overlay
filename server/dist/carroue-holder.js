e203b0ff026b21a8cb7694c1b6a78f36eae44558
Pimouki <allix1991@outlook.fr>
2025-04-29 16:01:45 +0200

    lol

--- server/dist/carroue-holder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setCarroueHolder = exports.carroueHolder = void 0;
-exports.carroueHolder = null;
-const setCarroueHolder = (name, haveTurn) => {
-    exports.carroueHolder = { name, haveTurn };
-};
-exports.setCarroueHolder = setCarroueHolder;
